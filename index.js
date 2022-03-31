d4fa3ee0054b45e9623f2be476fe0e42a8c87952
maxi-nm <em.nietomontoya@outlook.com>
2022-03-30 21:03:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const app = express();
+require("dotenv").config();
